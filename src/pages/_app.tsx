fc1b1ad3beeaa8065b299e747f05b400b89a2421
Mark van Aalst <markvanaalst@gmail.com>
2023-05-26 15:09:52 +0200

    Add preferred state to themebutton

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,30 @@
-import '@/styles/globals.css';
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
-import { ThemeProvider, useTheme } from 'next-themes';
-import { useEffect, useState } from 'react';
+import "@/styles/globals.css";
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { ThemeProvider, useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export default function App(props: AppProps & { colorScheme: ColorScheme }) {
-	const [colorScheme, setColorScheme] = useState<ColorScheme>('light');
-	const [mounted, setMounted] = useState(false);
-	const { theme, resolvedTheme } = useTheme();
-	const { Component, pageProps } = props;
+  const [colorScheme, setColorScheme] = useState<ColorScheme>("light");
+  const { Component, pageProps } = props;
 
-	// useEffect only runs on the client, so now we can safely show the UI
-	useEffect(() => {
-		setMounted(true);
-	}, []);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-	if (!mounted) {
-		return null;
-	}
-
-	const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
-	console.log(theme);
-
-	if (theme != undefined) theme == 'dark' ? toggleColorScheme('dark') : toggleColorScheme('light');
-
-	return (
-		<>
-			<Head>
-				<title>Page title</title>
-				<meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-			</Head>
-			<ThemeProvider attribute="class">
-				<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-					<MantineProvider theme={{ colorScheme }}>
-						<Component {...pageProps} />
-					</MantineProvider>
-				</ColorSchemeProvider>
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider attribute="class" enableSystem={true}>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+          <MantineProvider theme={{ colorScheme }}>
+            <Component {...pageProps} />
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </ThemeProvider>
+    </>
+  );
 }
